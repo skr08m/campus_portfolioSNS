c4a9f6ddeaf331a94ac219dee96ae213269b95ba
skr08m <sakuraba.mizuki08@gmail.com>
2025-11-11 15:21:32 +0900

    フォルダ分け、バックエンド環境構築

---
+++ backend/src/main/java/com/example/campus_portfolio/CampusPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusPortfolioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/campus_portfolio/CampusPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
