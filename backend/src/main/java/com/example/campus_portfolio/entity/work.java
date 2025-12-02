74b4e401fb8ada2654db85ee2cfb81e8cff2d8ae
ssaatoto <23fi047@ms.dendai.ac.jp>
2025-12-02 16:50:47 +0900

    work.javaの追加（文字数lengthどうしますか）

---
+++ backend/src/main/java/com/example/campus_portfolio/entity/work.java
@@ -0,0 +1,44 @@
+package com.example.campus_portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "work")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class work {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "work_id")
+    private Long work_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")  // 外部キー
+    private user user_id;
+
+    @Column(name = "work_name", length = 100)
+    private String work_name;
+
+    @Column(name = "work_upload_time")
+    private ZonedDateTime work_upload_time;
+
+    @Column(name = "title", length = 200)
+    private String title;
+
+    @Column(name = "explanation", columnDefinition = "TEXT")
+    private String explanation;
+
+    @Column(name = "repository_url", length = 300)
+    private String repository_url;
+
+    @Column(name = "work_storage_url", length = 300)
+    private String workStorage_url;
+
+    @Column(name = "work_extension", length = 20)
+    private String workExtension;
+}
