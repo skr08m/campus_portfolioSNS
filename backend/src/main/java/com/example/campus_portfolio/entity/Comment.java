b8343a372b5df025ca84dcc88cacba541d67e531
ssaatoto <23fi047@ms.dendai.ac.jp>
2025-12-07 07:04:34 +0900

    ファイルを作成しました

---
+++ backend/src/main/java/com/example/campus_portfolio/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.example.campus_portfolio.entity;
+
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="comment")
+
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long comment_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "work_id")
+    private Work work_id;
+
+    @Column(name = "comment_content", columnDefinition = "TEXT", length = 300)
+    private String comment_conten;
+
+    @Column(name = "sent_at")
+    private ZonedDateTime sent_at;
+}
---
+++ backend/src/main/java/com/example/campus_portfolio/entity/User_frequent_tags.java
@@ -0,0 +1,25 @@
+package com.example.campus_portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="user_frequent_tags")
+
+public class User_frequent_tags {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "frequent_tag_id")
+    private Long frequent_tag_id;
+ 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user_id;   
+}
