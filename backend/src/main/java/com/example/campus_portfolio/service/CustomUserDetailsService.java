a61bd7fea6f0692e03e891b9e05eb47b0cacc7ba
skr08m <sakuraba.mizuki08@gmail.com>
2025-12-09 15:42:22 +0900

    エラー修正

--- backend/src/main/java/com/example/campus_portfolio/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/campus_portfolio/service/CustomUserDetailsService.java
@@ -20,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findByMailAddress(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User.builder()
---
+++ backend/src/main/java/com/example/campus_portfolio/service/WorkService.java
@@ -0,0 +1,5 @@
+package com.example.campus_portfolio.service;
+
+public class WorkService {
+
+}
