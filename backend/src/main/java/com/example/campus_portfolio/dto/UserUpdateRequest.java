40665fa2e9fac0c78b95a846d847d4b398ea5473
ssaatoto <23fi047@ms.dendai.ac.jp>
2025-12-19 00:54:08 +0900

    ユーザ管理API

--- backend/src/main/java/com/example/campus_portfolio/dto/UserUpdateRequest.java
+++ backend/src/main/java/com/example/campus_portfolio/dto/UserUpdateRequest.java
@@ -1,5 +1,15 @@
 package com.example.campus_portfolio.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UserUpdateRequest {
-    //後で作ります
+    private String username; //名前
+    private String MailAddress; //メアド
+    private String selfIntroduction; //自己紹介
+    private String profilePhotoUrl; //プロフィール
 }
