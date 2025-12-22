2f256ffc6cd7a9d5c1a2ebd2b07d017b8d7fb49a
You829 <you829game@gmail.com>
2025-12-22 21:59:21 +0900

    WorkService.javaなどの修正

--- frontend/src/api/ApiCommunicationExample.jsx
+++ frontend/src/api/ApiCommunicationExample.jsx
@@ -223,10 +223,14 @@ class ApiCommunicationExample {
     // }
 
     static async removeFromAlbum(jwt, workId) {
-        const response = await fetch(`http://localhost:8080/api/works/${workId}/album`, {
-            method: "POST", // DELETE ではなく POST に変更してみる
-            headers: { "Authorization": `Bearer ${jwt}` }
+        const response = await fetch(`http://localhost:8080/api/works/${workId}/album/remove`, {
+            method: "POST", // Javaの @PostMapping に合わせる
+            headers: {
+                "Authorization": `Bearer ${jwt}`
+            }
         });
+
+        // 405エラーが出ないかチェック
         await this.checkResponse(response);
     }
 
